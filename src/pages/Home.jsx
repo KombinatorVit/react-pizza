14d2b319b7f471a5fab6dfd2fa082b18addeea1a
Vit Onischenko <slonik0775@gmail.com>
2022-11-19 00:04:06 +0200

    Оптимизируем поиск с помощью Debounce, пагинация через Redux Toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import axios  from "axios";
 import {Categories} from "../components/Categories";
 import {Sort} from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
@@ -7,18 +8,17 @@ import {useEffect, useState} from "react";
 import {Pagination} from "../components/Pagination";
 import {SearchContext} from "../App";
 import {useDispatch, useSelector} from "react-redux";
-import {setCategoryId} from "../redux/slices/filterSlice";
+import {setCategoryId, setCurrentPage} from "../redux/slices/filterSlice";
 
 export const Home = ({}) => {
 
-    const {categoryId, sort} = useSelector(state => state.filter)
+    const {categoryId, sort, currentPage} = useSelector(state => state.filter)
     const sortType = sort.sortProperty
     const dispatch = useDispatch()
 
     const [items, setItems] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
-    const [currentPage, setCurrentPage] = useState(1)
 
     const {searchValue} = useContext(SearchContext)
 
@@ -27,6 +27,9 @@ export const Home = ({}) => {
         dispatch(setCategoryId(id))
     }
 
+    const onChangePage = number => {
+        dispatch(setCurrentPage(number))
+    }
     useEffect(() => {
         setIsLoading(true)
         const sortBy = sortType.replace('-', '');
@@ -34,13 +37,9 @@ export const Home = ({}) => {
         const category = categoryId > 0 ? `category=${categoryId}` : '';
         const search = searchValue ? `search=${searchValue}` : '';
 
-        fetch(`https://6369227915219b8496105d27.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`).then((res) => {
-            return res.json()
-        })
-            .then((arr) => {
-                setItems(arr)
-                setIsLoading(false)
-            })
+
+        axios.get(`https://6369227915219b8496105d27.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`).then(res => {setItems(res.data)
+                    setIsLoading(false)})
         window.scrollTo(0, 0)
     }, [categoryId, sortType, searchValue, currentPage])
 
@@ -58,7 +57,7 @@ export const Home = ({}) => {
                     isLoading
                         ? [...new Array(6)].map((_, i) => <Skeleton key={i}/>) : pizzas}
             </div>
-            <Pagination currentPage={currentPage} onChangePage={(number) => setCurrentPage(number)}/>
+            <Pagination currentPage={currentPage} onChangePage={onChangePage}/>
         </div>
     );
 };
