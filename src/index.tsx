cc4fbae60c59d5682cc7f176d7600c8696e527bf
KombinatorVit <slonik0775@gmail.com>
2023-01-04 13:57:51 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import {Provider} from 'react-redux';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {store} from './redux/store';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
     <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')  as HTMLElement
+
+    </BrowserRouter>
 );
-
-
